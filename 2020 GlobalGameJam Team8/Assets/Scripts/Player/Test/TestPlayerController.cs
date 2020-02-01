cae6aa6ed8f852f9c4b0e7b5fdd84335f698ee65
Andy1013 <asd3465789@gmail.com>
2020-02-01 10:47:33 +0800

    fristSetup

---
+++ 2020 GlobalGameJam Team8/Assets/Scripts/Player/Test/TestPlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerController : MonoBehaviour
+{
+    public float speed=10;
+
+    public GameObject holdPoint;
+    public GameObject holdTube;
+    public bool isHold;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(horizontal, 0, vertical) * speed;
+
+        if (Mathf.Abs(horizontal) + Mathf.Abs(vertical) > 0.2)
+            transform.eulerAngles = new Vector3(0, Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg, 0);
+
+        if (isHold) HoldingTube();
+
+        if (Input.GetKeyDown(KeyCode.X) && isHold)
+        {
+            PutDownTube();
+            isHold = false;
+        }
+    }
+
+    public void HoldingTube()
+    {
+        holdTube.gameObject.transform.position = holdPoint.transform.position;
+    }
+
+    public void PutDownTube()
+    {
+        Vector3 putDownPos = transform.position + transform.forward;
+        holdTube.gameObject.transform.position = new Vector3 (Mathf.Round(putDownPos.x),1, Mathf.Round(putDownPos.z));
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Tube"))
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                isHold = true;
+                holdTube = collision.gameObject;
+            }
+        }
+    }
+}
