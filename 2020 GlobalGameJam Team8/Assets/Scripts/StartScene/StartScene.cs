f44dfff420081abe05c416fc120a045645c29148
Andy1013 <asd3465789@gmail.com>
2020-02-02 14:27:21 +0800

    UPDATE

---
+++ 2020 GlobalGameJam Team8/Assets/Scripts/StartScene/StartScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XboxCtrlrInput;
+
+public class StartScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (XCI.GetButtonDown(XboxButton.Start, XboxController.All))
+            SceneManager.LoadScene("PlayerMenu");
+    }
+}
