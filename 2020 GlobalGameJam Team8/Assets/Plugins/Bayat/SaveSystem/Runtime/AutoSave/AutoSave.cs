aa7f13b2dcde5bc07ff831aeffbda01c35ad6472
iantttianttt <iantttiantttianttt@gmail.com>
2020-02-29 16:51:05 +0800

    Add SaveSystem Plugin

---
+++ 2020 GlobalGameJam Team8/Assets/Plugins/Bayat/SaveSystem/Runtime/AutoSave/AutoSave.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Bayat.Json;
+
+namespace Bayat.SaveSystem
+{
+
+#if UNITY_EDITOR
+    [ExecuteInEditMode]
+#endif
+    [AddComponentMenu("Bayat/Save System/Auto Save")]
+    public class AutoSave : GameObjectSerializationHandler
+    {
+
+        protected virtual void OnEnable()
+        {
+            AutoSaveManager.Current.AddAutoSave(this);
+        }
+
+        protected virtual void OnDisable()
+        {
+            AutoSaveManager.Current.RemoveAutoSave(this);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            AutoSaveManager.Current.RemoveAutoSave(this);
+        }
+
+    }
+
+}
